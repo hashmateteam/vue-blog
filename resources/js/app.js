25d69f4ebfd2334cfbd0660b6b2f763c3839bf95
hashmateteam <hashmateteam@gmail.com>
2020-06-24 23:32:29 +0500

    import export problems

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,19 +4,23 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
-//Initialize Vue in window of browser
+//require('./bootstrap');
 window.Vue = require('vue');
+import shared from './shared/export.js';
+console.log(shared);
+require(shared);
+console.log(shared);
+//Initialize Vue in window of browser
+
 
 //Importing basic libraries
-import router from './router'
-import VueAxios from 'vue-axios';
-import axios from 'axios';
+//import router from './router'
+//import VueAxios from 'vue-axios';
+//import axios from 'axios';
 //import VueSocialauth from 'vue-social-auth';
-import VueCookie from 'vue-cookie';
-import moment from 'moment';
-import VueCarousel from 'vue-carousel';
+//import VueCookie from 'vue-cookie';
+//import moment from 'moment';
+//import VueCarousel from 'vue-carousel';
 //import editor from 'vue2-medium-editor';
 //import MediumEditor from 'medium-editor';
 
@@ -25,44 +29,44 @@ import VueCarousel from 'vue-carousel';
 //Object.defineProperty(Vue.prototype, '$editor', { value: MediumEditor });
 
 //Use or intitalize the basic libraries
-Vue.use(VueAxios, axios);
-Vue.use(VueCookie);
-Vue.use(VueCarousel);
+//Vue.use(VueAxios, axios);
+//Vue.use(VueCookie);
+//Vue.use(VueCarousel);
 //Vue.component('editor', { editor });
 //Shared State Veux store
-import store from "./store";
+//import store from "./store";
 
 //filters
-Vue.filter('format_date', function(value) {
-    if (value) {
-        return moment(String(value)).format('MMMM Do YYYY, h:mm:ss a');
-    }
-});
-Vue.filter('format_text', function(value) {
-    if (value) {
-        return value.substr(0, 45);
-    }
-});
+//Vue.filter('format_date', function(value) {
+//    if (value) {
+//        return moment(String(value)).format('MMMM Do YYYY, h:mm:ss a');
+//    }
+//});
+//Vue.filter('format_text', function(value) {
+//    if (value) {
+//        return value.substr(0, 45);
+//    }
+//});
 
 //directive two-ways
-Vue.directive('example', {
-        twoWay: true,
-        bind: function() {
-            this.handler = function() {
-                // set data back to the vm.
-                // If the directive is bound as v-example="a.b.c",
-                // this will attempt to set `vm.a.b.c` with the
-                // given value.
-                this.set(this.el.innerHTML)
-            }.bind(this)
-            this.el.addEventListener('input', this.handler)
-        },
-        unbind: function() {
-            this.el.removeEventListener('input', this.handler)
-        }
-    })
-    //Pages
-import Index from './pages/Index.vue';
+//Vue.directive('example', {
+//        twoWay: true,
+//        bind: function() {
+//           this.handler = function() {
+// set data back to the vm.
+// If the directive is bound as v-example="a.b.c",
+// this will attempt to set `vm.a.b.c` with the
+// given value.
+//                this.set(this.el.innerHTML)
+//            }.bind(this)
+//            this.el.addEventListener('input', this.handler)
+//        },
+//        unbind: function() {
+//            this.el.removeEventListener('input', this.handler)
+//        }
+//    })
+//Pages
+//import Index from './pages/Index.vue';
 /*
 //Components
 import AuthinWrapper from './interfaces/AuthinWrapper.vue';
@@ -121,14 +125,15 @@ Vue.use(VueSocialauth, {
 })
 */
 //Initialize the Vue app 
-const app = new Vue(Vue.util.extend({
-    router,
-    store
-}, Index)).$mount('#app');
+//const app = new Vue(Vue.util.extend({
+//    router,
+//    store
+//}, Index)).$mount('#app');
 
 //getting token from cookies before loading any component
-let token = app.$cookie.get("authentication_token");
-if (token !== null) {
-    app.$store.dispatch("update_token", String(token));
-    app.$store.dispatch("update_auth_user", Object(JSON.parse(app.$cookie.get("auth_user"))));
-}
+//let token = app.$cookie.get("authentication_token");
+//if (token !== null) {
+//    app.$store.dispatch("update_token", String(token));
+//    app.$store.dispatch("update_auth_user", Object(JSON.parse(app.$cookie.get("auth_user"))));
+//}
+console.log("rewiring is in process");
